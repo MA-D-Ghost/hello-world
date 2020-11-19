40b8afb99b60780ed729322bc87892706af2f722
MA-D-Ghost <72337112+MA-D-Ghost@users.noreply.github.com>
2020-11-19 19:36:16 +0700

    Add files via upload

---
+++ Tuan02_AI.py
@@ -0,0 +1,76 @@
+# Ho ten: Ton Thien Minh Anh
+# MSSV: 18110049
+# Bai thuc hanh Nhap Mon AI Tuan 2
+
+
+class Node:
+    goal_state=[0,0,0]
+    num_of_instances=0
+    def __init__(self,state,parent,action,depth):
+        self.parent=parent
+        self.state=state
+        self.action = action
+        self.depth = depth
+
+        Node.num_of_instances+=1
+
+    def goal_state(self):
+        if self.state == self.goal_state:
+            return True
+        return False
+    
+    def is_valid(self):
+        missionaries = self.state[0]
+        cannibals = self.state[1]
+        boat = self.state[2]
+        if missionaries < 0 or missionaries >3:
+            return False
+        if cannibals <0 or cannibals >3:
+            return False
+        if boat > 1 or boat <0:
+            return False
+        return True
+
+    def is_killed(self):
+        missionaries = self.state[0]
+        cannibals = self.state[1]
+        if missionaries <cannibals and missionaries > 0:
+            return True 
+        # Check for the other side 
+        if missionaries > cannibals and missionaries <3:
+            return True
+    
+    def generate_child(self):
+        children=[]
+        depth = self.depth + 1
+        op = -1 # Subtract
+        boat_move = "from left shore to right"
+        if self.state[2] ==0:
+            op = 1 # Add
+            boat_move = "from right shore to left"
+
+        for x in range(3):
+            for y in range(3):
+                # by_move = "Move %s missionaries and %s cannibals" %(x,y,boat_move)
+                new_state = self.state.copy()
+                new_state[0], new_state[1],new_state[2] = new_state[0] + op*x, new_state[1]+op*y,new_state[2]+op*1
+                action = [x,y,op]
+                new_node = Node(new_state,self,action,depth)
+                if x + y >= 1 and x+y <= 2:
+                    children.append(new_node)
+        return children
+
+        def find_solution(self):
+            solution = []
+            solution.append(self.action)
+            path = self 
+            while path.parent != None:
+                path = path.parent 
+                solution.append(path.action)
+            solution = solution[:-1]
+            solution.reverse()
+            return solution
+
+
+
+A = Node([3,3,1],None,None,0)
